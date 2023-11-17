a7e0c538a82787cec999547ad7a5ec3b312a2e87
FemkeFVB <149329792+FemkeFVB@users.noreply.github.com>
2023-11-17 15:10:05 +0100

    it works :)

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,6 +3,18 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def sin_tan_group():
+    pass
+
+@sin_tan_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Value we put in sine.",
+    show_default=True,  # show default in help
+)
 
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
@@ -10,6 +22,14 @@ def sin(number):
     print(df)
     return
 
+@sin_tan_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help="Value we put in tan.",
+    show_default=True,  # show default in help
+)
 
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
@@ -17,6 +37,5 @@ def tan(number):
     print(df)
     return
 
-
 if __name__ == "__main__":
-    sin(10)
+    sin_tan_group()
