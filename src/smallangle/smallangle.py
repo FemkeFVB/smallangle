034a03ebf5a7e01518870b6fc758fa6cde651bf9
FemkeFVB <149329792+FemkeFVB@users.noreply.github.com>
2023-11-17 16:17:31 +0100

    smallangle met docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,16 +1,21 @@
 # Femke van Balen 14004089
 # 17-11-2023
 # Programme that calculates either the sin or the tangent,
-# including the usage of click. 
-
+# including the usage of click.
+# ! I purposely did not use comments, because of the usage of docstrings ! (asked a TA)
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
 @click.group()
-def sin_tan_group(): # click funcion
+def sin_tan_group():
+    """This function puts the sine and tangent together in 1 group. It is now possible to
+    call either sin or tan, using seperate commands.
+    """
     pass
+
+help(sin_tan_group)
 
 @sin_tan_group.command()
 @click.option(
@@ -18,15 +23,23 @@ def sin_tan_group(): # click funcion
     "--number",
     default=1,
     help="Value we put in sine.",
-    show_default=True,  # show default in help
+    show_default=True,  
 )
 
-# calculate sine for 'n' steps
 def sin(number):
+    """This function calculates the value of the sine for 'n' steps, between 0 and 2pi.
+    You can choose a specific value for n by typing it in the terminal using the click writing format.
+
+    Args:
+        number (_type_): int
+    
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
+
+help(sin)
 
 @sin_tan_group.command()
 @click.option(
@@ -34,15 +47,23 @@ def sin(number):
     "--number",
     default=1,
     help="Value we put in tan.",
-    show_default=True,  # show default in help
+    show_default=True,  
 )
 
-# calculate tangent for 'n' steps
 def tan(number):
+    """This function calculates the value of the tangent for 'n' steps, between 0 and 2pi.
+    You can choose a specific value for n by typing it in the terminal using the click writing format.
+
+    Args:
+        number (_type_): int
+    """
+    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
+
+help(tan)
 
 if __name__ == "__main__":
     sin_tan_group()
