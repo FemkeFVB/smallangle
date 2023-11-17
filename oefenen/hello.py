12ccf5dd307880a331b24e341e1d2657e5ede314
FemkeFVB <149329792+FemkeFVB@users.noreply.github.com>
2023-11-17 14:10:21 +0100

    updated printing names
    Now the name Alice is printed 5 times, but in between each print, there is a small time pause.

--- oefenen/hello.py
+++ oefenen/hello.py
@@ -1,4 +1,5 @@
 import click 
+import time
 
 
 @click.command()
@@ -7,11 +8,21 @@ import click
     "-c",
     "--count",
     default=1,
+    help="Number of times to print greeting.",
+    show_default=True,  # show default in help
+)
+@click.option(
+    "-t",
+    "--time_pause",
+    default=0.0,
+    help="A time pause between print statements",
+    show_default = True
 )
 
-def hello(name, count):
+def hello(name, count, time_pause):
     for _ in range(count):
         print(f"Hello {name}!")
+        time.sleep(time_pause)
     
 
 if __name__ == "__main__":
